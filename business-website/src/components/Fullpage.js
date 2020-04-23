4cda83c972624865670f1f042f5334a7c961ea8b
nelsonk97 <nelsonkhiev@gmail.com>
2020-04-23 01:09:52 -0400

    Boilerplate

--- business-website/src/components/Fullpage.js
+++ business-website/src/components/Fullpage.js
@@ -11,25 +11,31 @@ function Fullpage() {
       render={({ state, fullpageApi }) => {
         return (
           <ReactFullpage.Wrapper>
-            <div className="section">
-              <p>Section 1 (welcome to fullpage.js)</p>
+            <div className="section" id="section-1">
+              <p>
+                Section 1 (please refer to{" "}
+                <a href="https://github.com/alvarotrigo/fullPage.js/#fullpagejs">
+                  fullpage.js docs
+                </a>
+                )
+              </p>
               <button onClick={() => fullpageApi.moveSectionDown()}>
                 Click me to move down
               </button>
             </div>
-            <div className="section">
+            <div className="section" id="section-2">
               <p>Section 2</p>
               <button onClick={() => fullpageApi.moveSectionDown()}>
                 Click me to move down
               </button>
             </div>
-            <div className="section">
+            <div className="section" id="section-3">
               <p>Section 3</p>
               <button onClick={() => fullpageApi.moveSectionDown()}>
                 Click me to move down
               </button>
             </div>
-            <div className="section">
+            <div className="section" id="section-4">
               <p>Section 4</p>
               <button onClick={() => fullpageApi.moveTo(1, 2)}>
                 Click me to move all the way up

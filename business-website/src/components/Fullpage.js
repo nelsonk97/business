c6fb0e7af3292f95280cc8343b99efdbf48b943a
nelsonk97 <nelsonkhiev@gmail.com>
2020-04-23 23:50:58 -0400

    Updates

--- business-website/src/components/Fullpage.js
+++ business-website/src/components/Fullpage.js
@@ -1,6 +1,7 @@
 import React from "react";
 //import ReactDOM from "react-dom";
 import ReactFullpage from "@fullpage/react-fullpage";
+import hero from "../assets/hero.png";
 
 function Fullpage() {
   return (
@@ -13,31 +14,14 @@ function Fullpage() {
           <ReactFullpage.Wrapper>
             <div id="fullpage">
               <div className="section" id="section-1">
-                <p>
-                  Section 1 (please refer to{" "}
-                  <a href="https://github.com/alvarotrigo/fullPage.js/#fullpagejs">
-                    fullpage.js docs
-                  </a>
-                  )
-                </p>
-                <button onClick={() => fullpageApi.moveSectionDown()}>
-                  Click me to move down
-                </button>
+                <div className="hero">
+                  <img src={hero} alt="hero-img" id="hero-img"></img>
+                  <p id="hero-txt">Elevate Your Online Presence.</p>
+                </div>
               </div>
-              <div className="section" id="section-2">
-                <p>Section 2</p>
-                <button onClick={() => fullpageApi.moveSectionDown()}>
-                  Click me to move down
-                </button>
-              </div>
-              <div className="section" id="section-3">
-                <p>Section 3</p>
-                <button onClick={() => fullpageApi.moveSectionDown()}>
-                  Click me to move down
-                </button>
-              </div>
+              <div className="section" id="section-2"></div>
+              <div className="section" id="section-3"></div>
               <div className="section" id="section-4">
-                <p>Section 4</p>
                 <button onClick={() => fullpageApi.moveTo(1, 2)}>
                   Click me to move all the way up
                 </button>
--- business-website/src/components/Header.js
+++ business-website/src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "./logo.jpg";
+import logo from "../assets/logo.jpg";
 
 function Header() {
   return (
     <div id="header">
       <div id="logo-container">
-        <img src={logo} alt="logo" id="logo"></img>
+        <img src={logo} alt="logo" id="logo-img"></img>
       </div>
     </div>
   );

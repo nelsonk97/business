ca26ce32e2d65e2ba192dd0cba7899dbfa77297e
Parikh999 <59714790+Parikh999@users.noreply.github.com>
2020-04-24 14:55:43 -0400

    Menu Icon
    Hamburger Menu Icon Added

--- business-website/src/components/Header.js
+++ business-website/src/components/Header.js
@@ -1,14 +1,19 @@
 import React from "react";
 import logo from "../assets/logo.jpg";
 
-function Header() {
-  return (
-    <div id="header">
-      <div id="logo-container">
-        <img src={logo} alt="logo" id="logo-img"></img>
+class Header extends React.Component {
+  render() {
+    return(
+      <div>
+        <div id="header">
+          <div id="logo-container">
+            <img src={logo} alt="logo" id="logo-img"></img>
+            <div id="menu"><i class="fas fa-bars fa-2x"></i></div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default Header;

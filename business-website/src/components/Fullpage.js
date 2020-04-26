8a349dc23722de99d6e2277a514606ffbfd19900
nelsonk97 <nelsonkhiev@gmail.com>
2020-04-26 15:58:28 -0400

    section 3

--- business-website/src/components/Fullpage.js
+++ business-website/src/components/Fullpage.js
@@ -1,7 +1,7 @@
 import React from "react";
 //import ReactDOM from "react-dom";
 import ReactFullpage from "@fullpage/react-fullpage";
-// import hero from "../assets/hero-text-1c.png";
+import hero from "../assets/hero-text-1e.png";
 
 function Fullpage() {
   return (
@@ -15,70 +15,124 @@ function Fullpage() {
             <div id="fullpage">
               <div className="section" id="section-1">
                 <div className="hero">
-                  {/* <img src={hero} alt="hero" id="hero-txt"></img> */}
-                  <p id="hero-txt">7ven digital media</p>
+                  <img src={hero} alt="hero" id="hero-logo"></img>
                   <p id="hero-subtxt">Elevate your online presence.</p>
                 </div>
               </div>
               <div className="section" id="section-2">
-                <div className="info-container" id="info-container">
+                <div className="info-container-1" id="info-container-1">
                   <div className="info-1" id="info-1">
-                    <p id="info-1-header">Challenge.</p>
-                    <p id="info-1-subtxt">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                    <p id="info-1-header">Modern.</p>
-                    <p id="info-1-subtxt">
-                      Faucibus purus in massa tempor nec. Ut faucibus pulvinar
-                      elementum integer enim neque. Malesuada proin libero nunc
-                      consequat interdum varius sit amet. Viverra justo nec
-                      ultrices dui sapien eget. A diam sollicitudin tempor id eu
-                      nisl nunc. Ullamcorper malesuada proin libero nunc
-                      consequat interdum varius sit.
-                    </p>
-                    <p id="info-1-header">Elevate.</p>
-                    <p id="info-1-subtxt">
-                      Malesuada proin libero nunc consequat interdum varius sit
-                      amet. Tempus imperdiet nulla malesuada pellentesque elit
-                      eget. Blandit turpis cursus in hac. Amet nisl purus in
-                      mollis. A diam sollicitudin tempor id eu nisl nunc. Enim
-                      sit amet venenatis urna. Egestas diam in arcu cursus
-                      euismod quis viverra nibh cras.
-                    </p>
+                    <div className="info-1-blob">
+                      <p id="info-1-header">Lorem.</p>
+                      <p id="info-1-subtxt">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat.
+                      </p>
+                    </div>
+                    <div className="info-1-blob">
+                      <p id="info-1-header">Ipsum.</p>
+                      <p id="info-1-subtxt">
+                        Faucibus purus in massa tempor nec. Ut faucibus pulvinar
+                        elementum integer enim neque. Malesuada proin libero
+                        nunc consequat interdum varius sit amet. Viverra justo
+                        nec ultrices dui sapien eget. A diam sollicitudin tempor
+                        id eu nisl nunc. Ullamcorper malesuada proin libero nunc
+                        consequat interdum varius sit.
+                      </p>
+                    </div>
+                    <div className="info-1-blob">
+                      <p id="info-1-header">Dolor.</p>
+                      <p id="info-1-subtxt">
+                        Malesuada proin libero nunc consequat interdum varius
+                        sit amet. Tempus imperdiet nulla malesuada pellentesque
+                        elit eget. Blandit turpis cursus in hac. Amet nisl purus
+                        in mollis. A diam sollicitudin tempor id eu nisl nunc.
+                        Enim sit amet venenatis urna. Egestas diam in arcu
+                        cursus euismod quis viverra nibh cras.
+                      </p>
+                    </div>
                   </div>
                   <div className="info-2" id="info-2">
-                    <p id="info-1-header">Challenge.</p>
-                    <p id="info-1-subtxt">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                    <p id="info-1-header">Modern.</p>
-                    <p id="info-1-subtxt">
-                      Faucibus purus in massa tempor nec. Ut faucibus pulvinar
-                      elementum integer enim neque. Malesuada proin libero nunc
-                      consequat interdum varius sit amet. Viverra justo nec
-                      ultrices dui sapien eget. A diam sollicitudin tempor id eu
-                      nisl nunc. Ullamcorper malesuada proin libero nunc
-                      consequat interdum varius sit.
-                    </p>
-                    <p id="info-1-header">Elevate.</p>
-                    <p id="info-1-subtxt">
-                      Malesuada proin libero nunc consequat interdum varius sit
-                      amet. Tempus imperdiet nulla malesuada pellentesque elit
-                      eget. Blandit turpis cursus in hac. Amet nisl purus in
-                      mollis. A diam sollicitudin tempor id eu nisl nunc. Enim
-                      sit amet venenatis urna. Egestas diam in arcu cursus
-                      euismod quis viverra nibh cras.
-                    </p>
+                    <div className="info-1-blob">
+                      <p id="info-1-header">Lorem.</p>
+                      <p id="info-1-subtxt">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat.
+                      </p>
+                    </div>
+                    <div className="info-1-blob">
+                      <p id="info-1-header">Ipsum.</p>
+                      <p id="info-1-subtxt">
+                        Faucibus purus in massa tempor nec. Ut faucibus pulvinar
+                        elementum integer enim neque. Malesuada proin libero
+                        nunc consequat interdum varius sit amet. Viverra justo
+                        nec ultrices dui sapien eget. A diam sollicitudin tempor
+                        id eu nisl nunc. Ullamcorper malesuada proin libero nunc
+                        consequat interdum varius sit.
+                      </p>
+                    </div>
+                    <div className="info-1-blob">
+                      <p id="info-1-header">Dolor.</p>
+                      <p id="info-1-subtxt">
+                        Malesuada proin libero nunc consequat interdum varius
+                        sit amet. Tempus imperdiet nulla malesuada pellentesque
+                        elit eget. Blandit turpis cursus in hac. Amet nisl purus
+                        in mollis. A diam sollicitudin tempor id eu nisl nunc.
+                        Enim sit amet venenatis urna. Egestas diam in arcu
+                        cursus euismod quis viverra nibh cras.
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="section" id="section-3"></div>
+              <div className="section" id="section-3">
+                <div className="info-container-3" id="info-container-3">
+                  <div className="info-3" id="info-3">
+                    <div className="info-3-blob">
+                      <p id="info-3-header">Lorem.</p>
+                      <p id="info-3-subtxt">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="info-3" id="info-3">
+                    <div className="info-3-blob">
+                      <p id="info-3-header">Ipsum.</p>
+                      <p id="info-3-subtxt">
+                        Faucibus purus in massa tempor nec. Ut faucibus pulvinar
+                        elementum integer enim neque. Malesuada proin libero
+                        nunc consequat interdum varius sit amet. Viverra justo
+                        nec ultrices dui sapien eget. A diam sollicitudin tempor
+                        id eu nisl nunc. Ullamcorper malesuada proin libero nunc
+                        consequat interdum varius sit.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="info-3" id="info-3">
+                    <div className="info-3-blob">
+                      <p id="info-3-header">Dolor.</p>
+                      <p id="info-3-subtxt">
+                        Malesuada proin libero nunc consequat interdum varius
+                        sit amet. Tempus imperdiet nulla malesuada pellentesque
+                        elit eget. Blandit turpis cursus in hac. Amet nisl purus
+                        in mollis. A diam sollicitudin tempor id eu nisl nunc.
+                        Enim sit amet venenatis urna. Egestas diam in arcu
+                        cursus euismod quis viverra nibh cras.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div className="section" id="section-4">
                 <button onClick={() => fullpageApi.moveTo(1, 2)}>
                   Click me to move all the way up
--- business-website/src/components/Header.js
+++ business-website/src/components/Header.js
@@ -1,18 +1,20 @@
 import React from "react";
-import logo from "../assets/logo.jpg";
+import logo from "../assets/logo-2.png";
 
 class Header extends React.Component {
   render() {
-    return(
+    return (
       <div>
         <div id="header">
           <div id="logo-container">
             <img src={logo} alt="logo" id="logo-img"></img>
-            <div id="menu"><i class="fas fa-bars fa-2x"></i></div>
+            <div id="menu">
+              <i class="fas fa-bars fa-2x"></i>
+            </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 

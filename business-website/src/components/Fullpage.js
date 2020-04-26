18e1347f69fa0411a6c661e0941ee39276591439
nelsonk97 <nelsonkhiev@gmail.com>
2020-04-25 20:42:13 -0400

    test

--- business-website/src/components/Fullpage.js
+++ business-website/src/components/Fullpage.js
@@ -17,16 +17,64 @@ function Fullpage() {
                 <div className="hero">
                   {/* <img src={hero} alt="hero" id="hero-txt"></img> */}
                   <p id="hero-txt">7ven digital media</p>
-                  <p id="hero-subtxt">Elevate your online presence</p>
+                  <p id="hero-subtxt">Elevate your online presence.</p>
                 </div>
               </div>
               <div className="section" id="section-2">
                 <div className="info-container" id="info-container">
                   <div className="info-1" id="info-1">
-                    1
+                    <p id="info-1-header">Challenge.</p>
+                    <p id="info-1-subtxt">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <p id="info-1-header">Modern.</p>
+                    <p id="info-1-subtxt">
+                      Faucibus purus in massa tempor nec. Ut faucibus pulvinar
+                      elementum integer enim neque. Malesuada proin libero nunc
+                      consequat interdum varius sit amet. Viverra justo nec
+                      ultrices dui sapien eget. A diam sollicitudin tempor id eu
+                      nisl nunc. Ullamcorper malesuada proin libero nunc
+                      consequat interdum varius sit.
+                    </p>
+                    <p id="info-1-header">Elevate.</p>
+                    <p id="info-1-subtxt">
+                      Malesuada proin libero nunc consequat interdum varius sit
+                      amet. Tempus imperdiet nulla malesuada pellentesque elit
+                      eget. Blandit turpis cursus in hac. Amet nisl purus in
+                      mollis. A diam sollicitudin tempor id eu nisl nunc. Enim
+                      sit amet venenatis urna. Egestas diam in arcu cursus
+                      euismod quis viverra nibh cras.
+                    </p>
                   </div>
                   <div className="info-2" id="info-2">
-                    2
+                    <p id="info-1-header">Challenge.</p>
+                    <p id="info-1-subtxt">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <p id="info-1-header">Modern.</p>
+                    <p id="info-1-subtxt">
+                      Faucibus purus in massa tempor nec. Ut faucibus pulvinar
+                      elementum integer enim neque. Malesuada proin libero nunc
+                      consequat interdum varius sit amet. Viverra justo nec
+                      ultrices dui sapien eget. A diam sollicitudin tempor id eu
+                      nisl nunc. Ullamcorper malesuada proin libero nunc
+                      consequat interdum varius sit.
+                    </p>
+                    <p id="info-1-header">Elevate.</p>
+                    <p id="info-1-subtxt">
+                      Malesuada proin libero nunc consequat interdum varius sit
+                      amet. Tempus imperdiet nulla malesuada pellentesque elit
+                      eget. Blandit turpis cursus in hac. Amet nisl purus in
+                      mollis. A diam sollicitudin tempor id eu nisl nunc. Enim
+                      sit amet venenatis urna. Egestas diam in arcu cursus
+                      euismod quis viverra nibh cras.
+                    </p>
                   </div>
                 </div>
               </div>

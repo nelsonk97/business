61724fde5a080385adf44e909e4dbac5a25041fd
nelsonk97 <nelsonkhiev@gmail.com>
2020-04-29 13:33:19 -0400

    update

--- business-website/src/components/Fullpage.js
+++ business-website/src/components/Fullpage.js
@@ -91,48 +91,20 @@ function Fullpage() {
                   </div>
                 </div>
               </div>
-              <div className="section" id="section-3">
+              {/*               <div className="section" id="section-3">
                 <div className="info-container-3" id="info-container-3">
-                  <div className="info-3" id="info-3">
-                    <div className="info-3-blob">
-                      <p id="info-3-header">Lorem.</p>
-                      <p id="info-3-subtxt">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="info-3" id="info-3">
-                    <div className="info-3-blob">
-                      <p id="info-3-header">Ipsum.</p>
-                      <p id="info-3-subtxt">
-                        Faucibus purus in massa tempor nec. Ut faucibus pulvinar
-                        elementum integer enim neque. Malesuada proin libero
-                        nunc consequat interdum varius sit amet. Viverra justo
-                        nec ultrices dui sapien eget. A diam sollicitudin tempor
-                        id eu nisl nunc. Ullamcorper malesuada proin libero nunc
-                        consequat interdum varius sit.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="info-3" id="info-3">
-                    <div className="info-3-blob">
-                      <p id="info-3-header">Dolor.</p>
-                      <p id="info-3-subtxt">
-                        Malesuada proin libero nunc consequat interdum varius
-                        sit amet. Tempus imperdiet nulla malesuada pellentesque
-                        elit eget. Blandit turpis cursus in hac. Amet nisl purus
-                        in mollis. A diam sollicitudin tempor id eu nisl nunc.
-                        Enim sit amet venenatis urna. Egestas diam in arcu
-                        cursus euismod quis viverra nibh cras.
-                      </p>
-                    </div>
-                  </div>
+                  <div className="info-3" id="info-3"></div>
+                  <div className="info-3" id="info-3"></div>
+                  <div className="info-3" id="info-3"></div>
+                  <div className="info-3" id="info-3"></div>
+                  <div className="info-3" id="info-3"></div>
+                  <div className="info-3" id="info-3"></div>
+                  <div className="info-3" id="info-3"></div>
+                  <div className="info-3" id="info-3"></div>
+                  <div className="info-3" id="info-3"></div>
+                  <div className="info-3" id="info-3"></div>
                 </div>
-              </div>
+              </div> */}
               <div className="section" id="section-4">
                 <button onClick={() => fullpageApi.moveTo(1, 2)}>
                   Click me to move all the way up

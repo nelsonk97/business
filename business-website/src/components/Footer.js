a0f866253483a1259882f463555ff05f46febe1e
nelsonk97 <nelsonkhiev@gmail.com>
2020-04-29 14:03:16 -0400

    update

--- business-website/src/components/Footer.js
+++ business-website/src/components/Footer.js
@@ -3,8 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <div id="footer">
-      powered by <span id="react-footer">react.js</span> +{" "}
-      <span id="fullpage-footer">fullpage.js</span>
+      <a>
+        powered by <span id="react-footer">react.js</span> +{" "}
+        <span id="fullpage-footer">fullpage.js</span>
+      </a>
     </div>
   );
 }
--- business-website/src/components/Fullpage.js
+++ business-website/src/components/Fullpage.js
@@ -16,46 +16,11 @@ function Fullpage() {
               <div className="section" id="section-1">
                 <div className="hero">
                   <img src={hero} alt="hero" id="hero-logo"></img>
-                  <p id="hero-subtxt">Elevate your online presence.</p>
                 </div>
               </div>
               <div className="section" id="section-2">
                 <div className="info-container-1" id="info-container-1">
                   <div className="info-1" id="info-1">
-                    <div className="info-1-blob">
-                      <p id="info-1-header">Lorem.</p>
-                      <p id="info-1-subtxt">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat.
-                      </p>
-                    </div>
-                    <div className="info-1-blob">
-                      <p id="info-1-header">Ipsum.</p>
-                      <p id="info-1-subtxt">
-                        Faucibus purus in massa tempor nec. Ut faucibus pulvinar
-                        elementum integer enim neque. Malesuada proin libero
-                        nunc consequat interdum varius sit amet. Viverra justo
-                        nec ultrices dui sapien eget. A diam sollicitudin tempor
-                        id eu nisl nunc. Ullamcorper malesuada proin libero nunc
-                        consequat interdum varius sit.
-                      </p>
-                    </div>
-                    <div className="info-1-blob">
-                      <p id="info-1-header">Dolor.</p>
-                      <p id="info-1-subtxt">
-                        Malesuada proin libero nunc consequat interdum varius
-                        sit amet. Tempus imperdiet nulla malesuada pellentesque
-                        elit eget. Blandit turpis cursus in hac. Amet nisl purus
-                        in mollis. A diam sollicitudin tempor id eu nisl nunc.
-                        Enim sit amet venenatis urna. Egestas diam in arcu
-                        cursus euismod quis viverra nibh cras.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="info-2" id="info-2">
                     <div className="info-1-blob">
                       <p id="info-1-header">Lorem.</p>
                       <p id="info-1-subtxt">
